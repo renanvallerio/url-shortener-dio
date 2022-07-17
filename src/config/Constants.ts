f071d91500d8cade6ce6d5ddcd4f83bc6d5ca789
renanvallerio <renanvallerio@gmail.com>
2022-07-16 22:26:57 -1000

    complete

---
+++ src/config/Constants.ts
@@ -0,0 +1,7 @@
+export const config = {
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION:
+    'mongodb+srv://jester:wanderer@url-shortner-dio.jnoyd.mongodb.net/?retryWrites=true&w=majority',
+}
+
+//  'mongodb+srv://jester:wanderer@url-shortner-dio.jnoyd.mongodb.net/url-shortner-dio?retryWrites=true&w=majority',
